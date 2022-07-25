47518ee0ca68a20246b5152ac66599bb6c67df8f
Cristian Vidal <73236553+cristiangvidal99@users.noreply.github.com>
2022-07-25 12:31:33 -0300

    carpeta tabla

---
+++ Tablas/dbo.Datos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Datos] (
+    [ID]     INT           IDENTITY (1, 1) NOT NULL,
+    [Nombre] NVARCHAR (50) NULL,
+    [Clase]  NVARCHAR (50) NULL,
+    [Raza]   NVARCHAR (50) NULL,
+    [Nivel]  NVARCHAR (50) NULL
+);
+
